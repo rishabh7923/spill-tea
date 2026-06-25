55442f08362bc08470cc638607bc717d9a1362d4
rishabh7923 <rishabhyadav7923@gmail.com>
2026-06-26 03:35:21 +0530

    backend available on network

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,6 @@ AppDataSource.initialize()
     if (process.env.SEED) seedCategories();
   })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(+process.env.PORT || 3000, "0.0.0.0", () => {
   console.log('Server is running on port ' + (process.env.PORT || 3000));
 });
